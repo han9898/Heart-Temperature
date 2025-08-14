e8ebd2c15be902df47163bc42b665afd90537ad5
sohee <topa11uu@gmail.com>
2025-08-14 16:33:57 +0900

    ✏️ Chore : Next.js 추가

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import Page from "./page";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="Ko">
+      <body>
+        <Page />
+      </body>
+    </html>
+  );
+}
